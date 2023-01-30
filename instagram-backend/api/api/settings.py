1001ced0118f8cbc4e99199dc67764713fbaa42a
xeqtrTC <anunnakifoxtgtb2@gmail.com>
2023-01-30 15:28:45 +0100

    fix

--- instagram-backend/api/api/settings.py
+++ instagram-backend/api/api/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zg^_q$y_qs-qto#s)@sp0$*=u*o791aya9q%5=0_(&fzaazwql'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
